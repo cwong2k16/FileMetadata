1f51bff0f50f8fa6fea85a0fb206ee83663542db
cwong2015 <sirhc8002@gmail.com>
2017-12-26 21:48:30 -0500

    file metadata microservice finished

--- app.js
+++ app.js
@@ -3,8 +3,23 @@ var multer = require('multer');
 
 var app = express();
 
-app.get('/', function(req, res){
+var storage = multer.memoryStorage()
+var upload = multer({ storage: storage })
+
+app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/upload', upload.single('file'), function (req, res, next) {
+    var metadata = {size: null};
+    if(!req.file){
+        console.log("Please submit a file!");
+        res.json();
+    }
+    else{
+        metadata.size = req.file.size;
+        res.json(metadata);
+    }
 });
 
 app.listen(3008);
